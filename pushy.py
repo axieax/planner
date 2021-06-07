61570a41fd2b5baddf387da26b236d35bc7eb606
Peter Derias <peter_derias@hotmail.com>
2021-06-07 15:45:36 +1000

    added a likely buggy version of iterative deepening

--- pushy.py
+++ pushy.py
@@ -1,7 +1,8 @@
 from copy import deepcopy
 from numpy import average, var as variance
+from itertools import combinations
 from util import Course, Graph, PriorityQueue, course_level, relevant_prereqs_filter
-from data import courses, plan, plan_specs, all_courses
+from data import NUM_TERMS, courses, plan, plan_specs, all_courses
 
 def main(plan, plan_specs, selected_course_codes, find_optimal):
     '''
@@ -38,42 +39,91 @@ def main(plan, plan_specs, selected_course_codes, find_optimal):
 
     # set up a graph modelling course prerequisites and dependencies for the selected courses
     prereqs = Graph(selected_courses.values())
-
+    long = 0
     # create a priority queue for course placement
     pq = PriorityQueue()
 
-    # PLACEMENT PART 1: Place unplaced courses with dependencies into the priority queue
+    # PLACEMENT PART 1: Place all unplaced courses into the priority queue
     for course_code in unplaced_course_codes:
-        if len(prereqs.immediate_dependencies(course_code)) > 0:
-            pq.push(prereqs, selected_courses[course_code])
+        pq.push(prereqs, selected_courses[course_code])
+    
+    if long == 1:
+        # create a backtracking queue based on the possible courses, and allow for an iterating search 
+        backpq = []
+        while not pq.empty():
+            backpq.append(pq.pop())
+        
+        for i in range(20):
+            plan = [[] for _ in range(i)] # we will have to change this into a nicer object possibly, to accomodate preferences and stuff
+            recursive_check(backpq, 0, 0, plan)
 
-    plan = [[] for _ in range(20)]
-    for index, term in enumerate(plan):
-        term_number = index % 4
-        if term_number == 0:
-            continue
-        #print(f'Term: {term_number}')
-        refused = []
-        unsatisfied = set([x[3].code for x in pq.queue])
 
-        while len(term) != 3:
-            if pq.empty():
-                break
                 
-            course = pq.pop()
-            # check prereqs satisfied
-            if term_number in course.terms and (all(not x for x in course.prereqs) or any(unsatisfied & set(comb) == set() for comb in course.prereqs)):
-                term.append(course.code)
-            else:
-                refused.append(course)
-        #print("refused list")
-        for x in refused:
-            # print(x)
-            pq.push(prereqs, x)
+    else:
+
+
+        plan = [[] for _ in range(20)]
+        for index, term in enumerate(plan):
+            term_number = index % NUM_TERMS
+            if term_number == 0:
+                continue
+            #print(f'Term: {term_number}')
+            refused = []
+            unsatisfied = set([x[3].code for x in pq.queue])
+
+            while len(term) != 3:
+                if pq.empty():
+                    break
+                    
+                course = pq.pop()
+                # check prereqs satisfied
+                if term_number in course.terms and (all(not x for x in course.prereqs) or any(unsatisfied & set(comb) == set() for comb in course.prereqs)):
+                    term.append(course.code)
+                else:
+                    refused.append(course)
+            #print("refused list")
+            for x in refused:
+                # print(x)
+                pq.push(prereqs, x)
     print(plan)
     return
     
-    
+def recursive_check(backpq, index, term, plan):
+    rejected = []
+    possible = []
+    term_number = index % 4
+    # anything in the ordered course list minus the stuff in the plan
+    unsatisfied = set([x[3].code for x in backpq])
+    for t in plan:
+        for c in t:
+            unsatisfied.remove(c)
+    if term_number == 0:
+        return # we avoid summer for now, we can bring it back in. Notice - we need to make this more flexible by defining some "avoid" terms and "normal" terms. This nicely generalises.
+    for course in backpq:
+        # check prereqs satisfied
+        if term_number in course.terms and (all(not x for x in course.prereqs) or any(unsatisfied & set(comb) == set() for comb in course.prereqs)):
+            possible.append(course)
+        else:
+            rejected.append(course)
+    # now i have a list of all the possible courses i can have this term, in a prioritised order. I can now make a combination list of all the possible arrangements for the term
+    poss_term = combinations(possible, 3)
+
+
+    # check how we went in this term
+    if possible.empty() and rejected.empty():
+        # valid plan found
+        print(plan)
+        exit()
+    for comb in poss_term:
+        plan[index] = list(comb)
+        for c in comb:
+            for string in comb:
+                backpq.remove(string)
+            recursive_check(backpq, index + 1, term + 1, plan)
+            for string in comb:
+                backpq.append(string)
+    return
+
     # place courses from priority queue
     plan = plan_placement(plan, plan_specs, selected_courses, unplaced_course_codes, pq)
 
