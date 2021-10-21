c19776508b06905cbc436d51d98c5583cd4f430a
Peter Derias <peter_derias@hotmail.com>
2021-10-21 20:37:32 +1100

    changed or cond

--- src/utils/logic.py
+++ src/utils/logic.py
@@ -1,7 +1,7 @@
 class And:
     """And Condition"""
 
-    def __init__(self, args) -> None:
+    def __init__(self, args: list) -> None:
         self.args = args
 
     def is_satisfied(self, plan, term_place) -> bool:
@@ -11,9 +11,8 @@ class And:
 class Or:
     """Or Condition"""
 
-    def __init__(self, lhs, rhs) -> None:
-        self.lhs = lhs
-        self.rhs = rhs
+    def __init__(self, args : list) -> None:
+        self.args = args
 
     def is_satisfied(self, plan, term_place) -> bool:
         return any([arg.is_satisfied(plan, term_place) for arg in self.args])
